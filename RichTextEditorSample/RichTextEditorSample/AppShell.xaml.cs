cbb2135ae193f47f68846ccc24573e7565750de1
snehakPV <sneha.sf4154@gmail.com>
2025-10-27 19:06:32 +0530

    Prepared sample for the MAUI Rich Text Editor control

---
+++ RichTextEditorSample/RichTextEditorSample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RichTextEditorSample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RichTextEditorSample/RichTextEditorSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace RichTextEditorSample
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
